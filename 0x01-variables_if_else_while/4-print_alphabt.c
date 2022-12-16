3d1e210a2b4195114e8cdd0741cd6fdbb4e48955
--replace-all <iamufutau@student.lautech.edu.ng>
2022-12-16 02:04:54 -0800

    print fixxed

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,22 +1,26 @@
 #include <stdio.h>
+
 /**
- * main - main block
- * Description: Get a random number and check its last digit, compare it with 5
+ * main - program that prints the alphabet in lowercase, followed by a new line
+ * You can only use the putchar
  * Return: 0
  */
+
 int main(void)
 {
-	char c + 'a';
-
-	while (c <= 'z')
-	{
-		if (c != 'e' && c != 'q')
-		{
-			putchar(c);
-		}
-		c++;
-	}
-
-	putchar('\n');
-	return (0);
+int lw = 'a';
+while (lw <= 'z')
+{
+if (lw == 'e' || lw == 'q')
+{
+lw += 1;
+}
+else
+{
+putchar(lw);
+lw += 1;
+}
+}
+putchar('\n');
+return (0);
 }
